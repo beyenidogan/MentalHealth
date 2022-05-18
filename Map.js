5b80c0d79726b139ae7946d26f57bb462a6ef4e5
byenidogan <yenidogan.berna@gmail.com>
2022-05-18 19:09:55 -0400

    changed the layout

--- Map.js
+++ Map.js
@@ -2,15 +2,16 @@ class Map {
 
     constructor(state, setGlobalState) {
         // initialize properties here
-        this.width = window.innerWidth * 1;
-        this.height = window.innerHeight * 0.6;        
-        this.margins = { top: 20, bottom: 30, left: 0, right: 50 };
+        this.width = window.innerWidth * 0.9;
+        this.height = window.innerHeight * 0.68;        
+        this.margins = { top: 20, bottom: 30, left: 100, right: 0 };
         this.countryjoin =[]
+        this.duration = 1000;
     
         this.svg = d3
           .select("#map-chart")
           .append("svg")
-          .attr("width", this.width-this.margins.right)
+          .attr("width", this.width)
           .attr("height", this.height);
     }
 
@@ -49,7 +50,7 @@ class Map {
             d3.min(filteredConditionData, function(d) { return d.Metric_Value; }),
             d3.max(filteredConditionData, function(d) { return d.Metric_Value; })
             ])
-            .range(["white","#f56e14"]);
+            .range(["#f56e14","white"]);
 
         var mapColor=d3.scaleLinear().domain([
             d3.min(filteredResourceData, function(d) { return d.Metric_Value; }),
@@ -74,6 +75,8 @@ class Map {
         const dot = this.svg.selectAll("circle")
           .data(filteredConditionData)
           .join("circle")
+          .transition()
+          .duration(this.duration/5) 
           .attr("class", "circle")
           .attr("fill",d => circleColor(d.Metric_Value))
           .attr("r", d=> z((d.Metric_Value)))
@@ -85,20 +88,19 @@ class Map {
            return `translate(${point[0]}, ${point[1]})`
           } )
         
-        dot.on("mouseover", function(d) {                                                            
+        dot.select("circle")
+        .on("mouseover", function(d) {                                                            
               console.log(d3.event.pageX)
-              d3.select("#map-chart")
-              .selectAll('.div.hover-content')
-                      .style("left", (d3.event.pageX-100)  + "px")
-                      .style("top", d3.event.pageY + "px")						
-                      .select("#events")
-                      .text(d.Country)
-                      .classed("visible", d=> d.visible)
-                      .style("fill", "white")
+              d3.select("#tooltip")
+                .style("left", (d3.event.pageX)  + "px")
+                .style("top", d3.event.pageY + "px")						
+                .select("#country")
+                .text(d.Country)
+                .style("fill", "white")
               d3.select("#tooltip")       
-                      .select("#month")
-                      .text(d3.timeFormat("%B")(d.Metric_Value))
-                      .style("fill", "white")
+                .select("#cases")
+                .text(d.Metric_Value)
+                .style("fill", "white")
               //Show the tooltip
               d3.select("#tooltip").classed("hidden", false);
               })  
